444e580944057c496364b3051dfa151757967a56
lakhanchandravanshi <lakhan@thoughtwin.com>
2025-06-16 12:25:06 +0530

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+
+import { createRoot } from 'react-dom/client'
+import { MantineProvider } from '@mantine/core';
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <MantineProvider>
+        <App />
+    </MantineProvider>
+)
+
+
+
